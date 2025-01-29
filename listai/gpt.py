7ac7298a161840f50c0b4dbbd1420cba2563ef91
masayoshiss <blindeexgt@gmail.com>
2025-01-29 18:47:12 +0200

    adding gpt file

---
+++ listai/gpt.py
@@ -0,0 +1,11 @@
+
+def add_three_numbers(a, b, c):
+    num_sum = a + b + c
+    return num_sum * 52
+
+
+my_val = add_three_numbers(2, 2, 2)
+print(my_val)
+
+my_val_sec = add_three_numbers(89, 52, 567)
+print(my_val_sec)
